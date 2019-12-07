861f38fc9f85db1b6e1f0c72cbc41d4c78d71a9d
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-12-07 10:13:22 -0800

    Remove duplicate handlers for built-ins

--- src/app/reexportr-src/reexportr.directive.ts
+++ src/app/reexportr-src/reexportr.directive.ts
@@ -11,7 +11,7 @@ import {
 @Directive({
   selector: "[reExportr]"
 })
-export class ReExportrDirective implements OnInit, OnDestroy {
+export class ReExportrDirective implements OnInit {
   context = {
     $implicit: undefined,
     reExportr: undefined
@@ -24,16 +24,9 @@ export class ReExportrDirective implements OnInit, OnDestroy {
     this.embeddedView = this._vcr.createEmbeddedView(this._tr, this.context);
   }
 
-  ngOnDestroy() {
-    this._vcr.clear();
-  }
-
   @Input()
   set reExportr(val: any) {
     this.context.reExportr = val;
     this.context.$implicit = val;
-    if (this.embeddedView) {
-      this.embeddedView.detectChanges();
-    }
   }
 }
